b05dcccc0377d9fb043f6f85e4ff48ba2c98f803
Qingyang Li <congel@gmail.com>
2023-09-25 11:03:48 -0700

    site config

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -10,20 +10,32 @@ interface ISiteMetadataResult {
 }
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
+  siteTitle: 'Conge 跑步',
+  siteUrl: 'https://conge.github.io',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
   description: 'Personal site and blog',
   navLinks: [
     {
-      name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
+      name: '主页',
+      url: 'https://conge.github.io',
     },
     {
-      name: 'About',
-      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+      name: '书影音',
+      url: 'https://conge.github.io/books/',
     },
-  ],
+    {
+      name: '跑步周记',
+      url: 'https://conge.github.io/category/#%E6%8A%98%E8%BF%94%E7%82%B9',
+    },
+    {
+      name: '育儿周记',
+      url: 'https://conge.github.io/category/#%E7%88%B6%E8%8C%83%E5%AD%A6%E5%A0%82',
+    },
+    {
+      name: '关于',
+      url: 'https://conge.github.io/about',
+    },
+],
 };
 
 export default data;
